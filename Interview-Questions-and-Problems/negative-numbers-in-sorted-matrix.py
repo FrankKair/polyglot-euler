48f85d6d1fb1491fe08df636d9e302147a4be9c7
Frank Kair <frankkair@gmail.com>
2017-02-27 16:35:51 -0300

    Corrects negative-numbers-in-sorted-array algorithm

--- Interview-Questions-and-Problems/negative-numbers-in-sorted-matrix.py
+++ Interview-Questions-and-Problems/negative-numbers-in-sorted-matrix.py
@@ -3,7 +3,7 @@
 columns = 4
 rows = 3
 matrix = [[0 for x in range(columns)] for y in range(rows)]
-matrix[0] = [-5, -2, 3, 5]
+matrix[0] = [-6, -3, -2, -1]
 matrix[1] = [-4, -1, 6, 7]
 matrix[2] = [7, 8, 9, 11]
 
@@ -23,14 +23,17 @@ def negative_numbers_in_sorted_matrix_2(matrix):
 	columns = len(matrix[0])
 	j = 0 # Column index
 	i = 0 # Row index
-	
-	while j < columns-1 and i < rows-1:
-		if matrix[i][j] > 0:
+
+	while j < columns and i < rows:
+		if matrix[i][j] < 0:
+			negative_count += 1
+			j += 1
+			if j == columns:
+				i += 1
+				j = 0
+		else:
 			i += 1 # Change row
 			j = 0 # Reset column
-		else:
-			negative_count += 1
-			j += 1 # Change column
 	return negative_count
 
 print negative_numbers_in_sorted_matrix(matrix)
