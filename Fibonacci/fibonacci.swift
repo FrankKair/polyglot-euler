966d620f57bdcdfc8bf21209ef99dc4eb4cc4a5f
Frank Kair <frankkair@gmail.com>
2017-03-04 12:33:04 -0300

    Adds Swift version

---
+++ Fibonacci/fibonacci.swift
@@ -0,0 +1,22 @@
+#!/usr/bin/env xcrun swift
+
+func fibonnaci(n: Int) -> Int {
+	if n < 2 {
+		return n
+	}
+	return fibonnaci(n: n-2) + fibonnaci(n: n-1)
+}
+
+func fibonnaciMemoized(n: Int) -> Int {
+    var cache = [0: 0, 1: 1]
+    if let number = cache[n] {
+        return number
+    }
+    cache[n] = fibonnaciMemoized(n: n-2) + fibonnaciMemoized(n: n-1)
+    return cache[n]!
+}
+
+if let argument = Int(CommandLine.arguments[1]) {
+	print(fibonnaci(n: argument))
+	print(fibonnaciMemoized(n: argument))
+}
