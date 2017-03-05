935cc6270e4c8cdd1fa875819b859527895cad34
Frank Kair <frankkair@gmail.com>
2017-03-05 19:02:32 -0300

    Adds Queue data structure (python)

---
+++ Data-Structures/Queue/Queue.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class Queue:
+	def __init__(self):
+		self.items = []
+
+	def is_empty(self):
+		return self.items == []
+
+	def enqueue(self, item):
+		self.items.insert(0, item)
+
+	def dequeue(self):
+		return self.items.pop()
+
+	def size(self):
+		return len(self.items)
