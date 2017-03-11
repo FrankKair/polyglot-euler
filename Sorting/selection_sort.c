7fbd6064e01fa6f6876137b37a2791c709620ab0
Frank Kair <frankkair@gmail.com>
2017-03-10 23:41:30 -0300

    Selection sort in C

---
+++ Sorting/selection_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void selection_sort(int A[], int n)
+{
+	for (int i = 0; i < sizeof(A)-1; i++)
+	{
+		int iMin = i;
+		for (int j = i+1; j < n; j++)
+		{
+			if (A[j] < A[iMin])
+			{
+				iMin = j;
+			}
+		}
+		int temp = A[i];
+		A[i] = A[iMin];
+		A[iMin] = temp;
+	}
+}
+
+// Example
+int main(int argc, char const *argv[])
+{
+	int A[] = {2, 7, 4, 1, 5, 3};
+	int size = (int) sizeof A / sizeof *A;
+	selection_sort(A, size);
+
+	// Prints elements
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d\n", A[i]);
+	}
+
+	return 0;
+}
