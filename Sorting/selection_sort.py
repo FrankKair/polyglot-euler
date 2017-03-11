7b31646c5cb8738672870f16f8a3aaf505fac426
Frank Kair <frankkair@gmail.com>
2017-03-11 00:10:23 -0300

    Selection sort in python

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def selection_sort(list):
+	for i in range(0, len(list)-1):
+		minimum = i
+		for j in range(i+1, len(list)):
+			if list[minimum] > list[j]:
+				minimum = j
+		temp = list[i]
+		list[i] = list[minimum]
+		list[minimum] = temp
+
+list = [2, 7, 4, 1, 5, 3]
+selection_sort(list)
+print list
