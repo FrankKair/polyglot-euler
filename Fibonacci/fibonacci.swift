393b6ec9700586e5b606ae7c09927a709e6a9269
Frank Kair <frankkair@gmail.com>
2017-06-18 18:01:46 -0300

    Modifies swift version to use guard

--- Fibonacci/fibonacci.swift
+++ Fibonacci/fibonacci.swift
@@ -1,16 +1,13 @@
 #!/usr/bin/env xcrun swift
-
 func fibonnaci(n: Int) -> Int {
-	if n < 2 {
-		return n
-	}
+	guard n > 1 else { return n }
 	return fibonnaci(n: n-2) + fibonnaci(n: n-1)
 }
 
 func fibonnaciMemoized(n: Int) -> Int {
     var cache = [0: 0, 1: 1]
-    if let number = cache[n] {
-        return number
+    if let number = cache[n] { 
+    	return number
     }
     cache[n] = fibonnaciMemoized(n: n-2) + fibonnaciMemoized(n: n-1)
     return cache[n]!
