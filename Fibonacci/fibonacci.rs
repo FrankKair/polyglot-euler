f3f2b69ca63ef61b74cf255a4174d412f95dbd1d
Frank Kair <frankkair@gmail.com>
2017-06-23 01:18:12 -0300

    Refactors Rust fibonacci

--- Fibonacci/fibonacci.rs
+++ Fibonacci/fibonacci.rs
@@ -2,20 +2,14 @@ use std::i64;
 use std::env;
 
 fn fibonacci(n: i64) -> i64 {
-	if n <= 2 {
-		return n;	
-	}
+	if n <= 2 { return n; }
 	return fibonacci(n-2) + fibonacci(n-1);
 }
 
-fn parse_input() {
-	if let Some(arg) = env::args().nth(1) {
+fn main() {
+    if let Some(arg) = env::args().nth(1) {
         if let Ok(x) = arg.parse::<i64>() {
         	println!("{}", fibonacci(x));
         }
     }
-}
-
-fn main() {
-    parse_input();
 }
