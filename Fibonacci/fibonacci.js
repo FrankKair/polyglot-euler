79ca9a3b37e8b80fc7721e2b8ad0dc07c17bebf2
Frank Kair <frankkair@gmail.com>
2017-06-28 08:29:23 -0300

    Adds ; to JavaScript fibonacci

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 function fib(x) {
-	if (x < 2) { return x }
-	return fib(x-1) + fib(x-2)
+	if (x < 2) { return x; }
+	return fib(x-1) + fib(x-2);
 }
 
-const arg = parseInt(process.argv.slice(2))
-console.log(fib(arg))
+const arg = parseInt(process.argv.slice(2));
+console.log(fib(arg));
