a39f00c61fbfe240a18e47503a1a3962ec83890a
Frank Kair <frankkair@gmail.com>
2017-06-29 21:43:19 -0300

    Omits named parameters in Swift Fibonacci

--- Fibonacci/fibonacci.swift
+++ Fibonacci/fibonacci.swift
@@ -1,15 +1,15 @@
 #!/usr/bin/env xcrun swift
-func fibonnaci(n: Int) -> Int {
+func fibonnaci(_ n: Int) -> Int {
 	guard n > 1 else { return n }
-	return fibonnaci(n: n-2) + fibonnaci(n: n-1)
+	return fibonnaci(n-2) + fibonnaci(n-1)
 }
 
-func fibonnaciMemoized(n: Int) -> Int {
+func fibonnaciMemoized(_ n: Int) -> Int {
     var cache = [0: 0, 1: 1]
     if let number = cache[n] { 
     	return number
     }
-    cache[n] = fibonnaciMemoized(n: n-2) + fibonnaciMemoized(n: n-1)
+    cache[n] = fibonnaciMemoized(n-2) + fibonnaciMemoized(n-1)
     return cache[n]!
 }
 
