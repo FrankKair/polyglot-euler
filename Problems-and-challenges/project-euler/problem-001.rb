ab892512e962876327790d3ffdd48d37f840d8f5
Frank Kair <frankkair@gmail.com>
2017-07-09 01:39:59 -0300

    Adds question to ruby file

--- Problems-and-challenges/project-euler/problem-001.rb
+++ Problems-and-challenges/project-euler/problem-001.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 sum = 0
 1000.times do |i|
   if i % 3 == 0 or i % 5 == 0
