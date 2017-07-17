1f23a62a53caf148c7067f38c3eef17760349a67
Frank Kair <frankkair@gmail.com>
2017-07-17 20:50:59 -0300

    Adds swift problem006

---
+++ Problems-and-challenges/project-euler/problem006.swift
@@ -0,0 +1,12 @@
+#!/usr/bin/env xcrun swift
+
+extension Int {
+    var squared: Int {
+        return self * self
+    }
+}
+
+let sumOfTheSquares = (0...100).map { $0 * $0 }.reduce(0, +)
+let squareOfTheSum = (0...100).reduce(0, +).squared
+
+print(squareOfTheSum - sumOfTheSquares)
