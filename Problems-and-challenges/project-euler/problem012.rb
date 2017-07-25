255620312de9953df6523c2f0dfd196f5dffd4b8
Frank Kair <frankkair@gmail.com>
2017-07-24 22:02:56 -0300

    Adds ruby problem012

---
+++ Problems-and-challenges/project-euler/problem012.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+class Integer
+  	def triangle_number
+    	(1..self).reduce(:+)
+  	end
+
+  	def factors
+  		(1..Math.sqrt(self)).select { |i| self % i == 0 }.reduce([]) do |list, number|
+  			list << number
+  			list << self / number unless self / number == number
+  			list
+  		end
+  	end
+end
+
+x = 1
+while x.triangle_number.factors.count < 500
+	x += 1
+end
+puts x.triangle_number
