6054ddfc6923b54b2563b29a865c5d3870b2ee83
Frank Kair <frankkair@gmail.com>
2017-07-27 23:25:11 -0300

    Adds naive ruby problem010

---
+++ Problems-and-challenges/project-euler/problem010.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+class Integer
+  	def prime?
+  		return false unless self > 1
+  		(2..Math.sqrt(self)).any? do |x|
+  			if x == self
+  				next
+  			end
+  			if self % x == 0
+  				return false
+  			end
+  		end
+  		true
+  	end
+end
+
+puts (0..2000000).select { |x| x.prime? }.reduce(:+)
