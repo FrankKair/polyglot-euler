603d6401b05fe43f20d7e732810670b255bc022b
Frank Kair <frankkair@gmail.com>
2017-07-29 13:18:50 -0300

    Optimizes a little swift problem010

--- Problems-and-challenges/project-euler/problem010.swift
+++ Problems-and-challenges/project-euler/problem010.swift
@@ -1,8 +1,11 @@
 #!/usr/bin/env xcrun swift
+extension Int {
+	var squareRootInt: Int { return Int(Float(self).squareRoot()) }
+}
 
 func generatePrimesUpTo(_ number: Int) -> [Int] {
     var listOfNumbers = Set(2...number)
-    for x in (2...number) {
+    for x in (2...number.squareRootInt) {
         let numbersToRemove = listOfNumbers.filter { $0 % x == 0 && $0 != x }
         if !numbersToRemove.isEmpty {
         	listOfNumbers.subtract(numbersToRemove)
