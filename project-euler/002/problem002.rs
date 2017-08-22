13ebcd91311e08d23ef0d9ea3796da3d8c761144
Frank Kair <frankkair@gmail.com>
2017-08-22 00:02:54 -0300

    Adds Rust problem002

---
+++ project-euler/002/problem002.rs
@@ -0,0 +1,16 @@
+fn main() {
+  let mut a = 1;
+  let mut b = 2;
+  let mut sum = 0;
+
+  while b < 4000000 {
+    if b % 2 == 0 {
+      sum += b;
+    }
+    let temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  println!("{:?}", sum);
+}
