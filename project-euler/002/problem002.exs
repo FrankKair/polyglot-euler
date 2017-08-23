bcff152d2e4aa3e5c089114cbbdcd39243028ba7
Frank Kair <frankkair@gmail.com>
2017-08-23 19:22:53 -0300

    Adds elixir problem002

---
+++ project-euler/002/problem002.exs
@@ -0,0 +1,15 @@
+#!/usr/bin/env elixir
+defmodule Problem002 do
+  defp fibonacci_sequence do
+    Stream.unfold({1, 1}, fn {a, b} -> {a, {b, a + b}} end)
+  end
+
+  def solve do
+    fibonacci_sequence()
+    |> Stream.take_while(&(&1 < 4000000))
+    |> Stream.filter(&(rem(&1, 2) == 0))
+    |> Enum.sum
+  end
+end
+
+IO.puts Problem002.solve
