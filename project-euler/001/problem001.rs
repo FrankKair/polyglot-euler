94063d2f30f9136deab21a76b9dcf8f0a8526c9d
Frank Kair <frankkair@gmail.com>
2017-08-28 18:38:14 -0300

    Uses sum instead of fold in Rust

--- project-euler/001/problem001.rs
+++ project-euler/001/problem001.rs
@@ -1,8 +1,8 @@
 fn main() {
-  let sum = (0..1000)
-            .into_iter()
-            .filter(|&x| x % 3 == 0 || x % 5 == 0)
-            .fold(0, |acc, x| acc + x);
+  let sum: i64 = (0..1000)
+                  .into_iter()
+                  .filter(|&x| x % 3 == 0 || x % 5 == 0)
+                  .sum();
 
   println!("{}", sum);
 }
