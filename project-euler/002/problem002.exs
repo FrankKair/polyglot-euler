103e4ddb4749a799089cf030f7ec9281fa869b65
Frank Kair <frankkair@gmail.com>
2017-08-29 13:36:10 -0300

    Uses Enum.reduce with (&+/2) instead of Enum.sum

--- project-euler/002/problem002.exs
+++ project-euler/002/problem002.exs
@@ -8,7 +8,7 @@ defmodule Problem002 do
     fibonacci_sequence()
     |> Stream.take_while(&(&1 < 4000000))
     |> Stream.filter(&(rem(&1, 2) == 0))
-    |> Enum.sum
+    |> Enum.reduce(&+/2)
   end
 end
 
