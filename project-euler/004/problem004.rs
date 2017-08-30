6ecce7619ee71157e917280f45194e02c7552e41
Frank Kair <frankkair@gmail.com>
2017-08-30 13:39:51 -0300

    Adds Rust problem004

---
+++ project-euler/004/problem004.rs
@@ -0,0 +1,17 @@
+fn is_palindrome(n: i64) -> bool {
+  let reversed: String = n.to_string().chars().rev().collect();
+  reversed == n.to_string()
+}
+
+fn main() {
+  let mut largest = 0;
+  for x in 100..999 {
+    for y in 100..999 {
+      let product = x * y;
+      if product > largest && is_palindrome(product) {
+        largest = product;
+      }
+    }
+  }
+  println!("{}", largest);
+}
