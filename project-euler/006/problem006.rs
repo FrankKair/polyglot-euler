bee06689552183e44911394bd21cb5aed18f1bbe
Frank Kair <frankkair@gmail.com>
2017-08-31 13:27:35 -0300

    Uses sum() instead of fold() in Rust problem006

--- project-euler/006/problem006.rs
+++ project-euler/006/problem006.rs
@@ -3,11 +3,11 @@ fn square(n: i64) -> i64 {
 }
 
 fn main() {
-  let sum_of_the_squares = (0..101)
-                            .map(|x| x * x)
-                            .fold(0, |acc, x| acc + x);
+  let sum_of_the_squares: i64 = (0..101)
+                                .map(|x| x * x)
+                                .sum();
 
-  let square_of_the_sum = square((0..101).fold(0, |acc, x| acc + x));
+  let square_of_the_sum = square((0..101).sum());
 
 
   println!("{}", square_of_the_sum - sum_of_the_squares);
