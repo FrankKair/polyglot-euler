de155ff86a1d5b23d45f19982bc0bf9443778134
Frank Kair <frankkair@gmail.com>
2017-09-02 23:42:55 -0300

    Refactors ruby problem005

--- project-euler/005/problem005.rb
+++ project-euler/005/problem005.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-number = (1..20).reduce do |acc, x|
-  acc.lcm(x)
-end
-
-# Smallest number evenly divisible by all numbers from 1 to 20.
-puts number
+puts (1..20).reduce { |acc, x| acc.lcm(x) }
