1d228e04a68c3aa27b43a03ba0ee1659dd4b8c6d
Frank Kair <frankkair@gmail.com>
2017-09-09 00:20:20 -0300

    Adds rust problem014

---
+++ project-euler/014/problem014.rs
@@ -0,0 +1,30 @@
+fn generate_collatz_sequence_starting_at(number: i64) -> Vec<i64> {
+  let mut numbers = vec![number];
+  let mut next = number;
+
+  while next != 1 {
+    if next % 2 == 0 {
+      next = next / 2;
+    } else {
+      next = 3 * next + 1;
+    }
+    numbers.push(next);
+  }
+  return numbers;
+}
+
+fn main() {
+  let mut longest_chain = Vec::new();
+  let mut number = 1;
+  let mut highest_number = 1;
+  
+  while number < 1000000 {
+    number += 1;
+    let chain = generate_collatz_sequence_starting_at(number);
+    if chain.len() > longest_chain.len() {
+      longest_chain = chain;
+      highest_number = number;
+    }
+  }
+  println!("{}", highest_number);
+}
