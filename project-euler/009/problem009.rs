7566b942c4d3699c86c875c4b340731cedf50a31
Frank Kair <frankkair@gmail.com>
2017-09-08 23:32:50 -0300

    Adds rust problem009

---
+++ project-euler/009/problem009.rs
@@ -0,0 +1,21 @@
+fn is_pythagorean_triplet(a: i64, b: i64, c: i64) -> bool {
+  a * a + b * b == c * c
+}
+
+fn main() {
+  let mut product = 0;
+
+  for a in 0..500 {
+    for b in 0..500 {
+      for c in 0..500 {
+        if is_pythagorean_triplet(a, b, c) {
+          let sum = a + b + c;
+          if sum == 1000 {
+            product = a * b * c;
+          }
+        }
+      }
+    }
+  }
+  println!("{}", product);
+}
