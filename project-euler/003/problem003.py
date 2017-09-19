9fb9c4e896228b7f909c3373116a4e175d8e42b7
Frank Kair <frankkair@gmail.com>
2017-09-18 22:26:30 -0300

    Adds python problem003

---
+++ project-euler/003/problem003.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import math
+
+def square_root_as_int(x):
+  return int(math.sqrt(x))
+
+def is_prime(number):
+  if number == 1:
+    return False
+  for x in range(2, square_root_as_int(number) + 1):
+    if x == number:
+      next
+    if number % x == 0:
+      return False
+  return True
+
+def factors_of_(number):
+  factors = []
+  for x in range(2, square_root_as_int(number) + 1):
+    if number % x == 0:
+      factors.append(x)
+      factors.append(number/x)
+  return factors
+
+factors = factors_of_(600851475143)
+primes = []
+for factor in factors:
+  if is_prime(factor):
+    primes.append(factor)
+print max(primes)
+print max(filter(lambda x: is_prime(x), factors_of_(600851475143)))
