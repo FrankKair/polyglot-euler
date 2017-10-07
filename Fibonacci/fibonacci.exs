98fd82e6364cede87689187cbe712bc71ad35824
Frank Kair <frankkair@gmail.com>
2017-10-07 18:25:51 -0300

    Refactors Elixir fibonacci

--- Fibonacci/fibonacci.exs
+++ Fibonacci/fibonacci.exs
@@ -1,8 +1,8 @@
 #!/usr/bin/env elixir
 defmodule Fib do 
-  def fib(0) do 0 end
-  def fib(1) do 1 end
-  def fib(n) do fib(n-1) + fib(n-2) end
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n), do: fib(n-1) + fib(n-2)
 end
 
 arg = String.to_integer(List.first(System.argv))
