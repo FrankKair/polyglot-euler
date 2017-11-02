cbf079759fd10f2dc19944bb4ae5b8fbfb3eab25
Frank Kair <frankkair@gmail.com>
2017-11-02 14:56:03 -0200

    Optimizes ruby problem010 using Prime module

--- project-euler/010/problem010.rb
+++ project-euler/010/problem010.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
-class Integer
-  def prime?
-    return false unless self > 1
-    (2..Math.sqrt(self)).any? do |x|
-      next if x == self
-      return false if (self % x).zero?
-    end
-    true
-  end
-end
-
-puts (0..2_000_000).select(&:prime?).reduce(:+)
+require 'mathn'
+puts Prime.take_while { |x| x < 2_000_000 }.reduce(:+)
