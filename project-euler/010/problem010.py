6dd6095b9e64eb41b4ae2d090efd5c2957976a78
Frank Kair <frankkair@gmail.com>
2017-11-04 17:54:09 -0200

    Optimizes python problem010 with prime generator and takewhile

--- project-euler/010/problem010.py
+++ project-euler/010/problem010.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
-import math
+
+from itertools import takewhile
 
 
-def square_root_as_int(x):
-    return int(math.sqrt(x))
+def primes():
+    D = {}
+    q = 2
+
+    while True:
+        p = D.pop(q, None)
+        if p:
+            x = p + q
+            while x in D:
+                x += p
+            D[x] = p
+        else:
+            D[q * q] = q
+            yield q
+        q += 1
 
 
-def generate_primes_up_to(number):
-    list_of_primes = set(range(2, number + 1))
-    for number in range(2, square_root_as_int(number) + 1):
-        numbers_to_remove = [
-            x for x in list_of_primes if x % number == 0 and x != number
-        ]
-        if len(numbers_to_remove) > 0:
-            list_of_primes.difference_update(numbers_to_remove)
-    return list_of_primes
-
-
-print(sum(generate_primes_up_to(2000000)))
+print sum(takewhile(lambda x: x < 2000000, primes()))
