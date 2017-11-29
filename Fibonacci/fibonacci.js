d7b8d44264a2becb43d5b996819b9cffda747942
Frank Kair <frankkair@gmail.com>
2017-11-28 23:47:56 -0200

    WIP New structure

--- Fibonacci/fibonacci.js
+++
@@ -1,7 +0,0 @@
-function fib(x) {
-  if (x < 2) { return x; }
-  return fib(x-1) + fib(x-2);
-}
-
-const arg = parseInt(process.argv.slice(2));
-console.log(fib(arg));
--- project-euler/001/problem001.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python
-print sum([x for x in range(0, 1000) if x % 3 == 0 or x % 5 == 0])
