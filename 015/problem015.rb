49d00bb6ddbab8ee9402b178de5afd493327e7cb
Frank Kair <frankkair@gmail.com>
2017-12-04 23:49:58 -0200

    Adds ruby problem015

---
+++ 015/problem015.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+class Integer
+  def factorial
+    return 1 if self.zero?
+    return 1 if self == 1
+    self * (self - 1).factorial
+  end
+end
+
+def combination(n, k)
+  n.factorial / (k.factorial * (n - k).factorial)
+end
+
+def count_routes(k)
+  combination(2 * k, k)
+end
+
+puts count_routes(20)
