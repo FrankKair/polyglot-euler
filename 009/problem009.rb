fb11f514891a665b7f84391ddf69f9f18755e8cb
Frank Kair <frankkair@gmail.com>
2017-12-06 15:31:43 -0200

    Refactors ruby problem009

--- 009/problem009.rb
+++ 009/problem009.rb
@@ -9,9 +9,12 @@ def pythagorean?(a, b, c)
   a.squared + b.squared == c.squared
 end
 
+product = 0
 (0..1000).each do |a|
   (a..1000).each do |b|
     c = 1000 - a - b
-    puts a * b * c if pythagorean?(a, b, c) && a + b + c == 1000
+    product = a * b * c if pythagorean?(a, b, c) && a + b + c == 1000
   end
 end
+
+puts product
