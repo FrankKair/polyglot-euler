3a37e931f3e1825dac032dbaeeb8962150a6e04e
Frank Kair <frankkair@gmail.com>
2017-12-05 22:24:30 -0200

    Adds Elixir problem025

---
+++ 025/problem025.exs
@@ -0,0 +1,14 @@
+#!/usr/bin/env elixir
+defmodule Problem025 do
+  defp fibonacci_sequence do
+    Stream.unfold({1, 1}, fn {a, b} -> {a, {b, a + b}} end)
+  end
+
+  def solve do
+    fibonacci_sequence()
+    |> Enum.find_index(&(length(Integer.digits(&1)) == 1000))
+    |> Kernel.+(1)
+  end
+end
+
+IO.puts Problem025.solve
