119db07e920f3eccb684ba59169e97cd449065ac
Frank Kair <frankkair@gmail.com>
2017-12-18 14:11:52 -0200

    Uses squared extension instead of closure in Swift problem006

--- 006/problem006.swift
+++ 006/problem006.swift
@@ -5,7 +5,7 @@ extension Int {
     }
 }
 
-let sumOfTheSquares = (0...100).map { $0 * $0 }.reduce(0, +)
+let sumOfTheSquares = (0...100).map { $0.squared }.reduce(0, +)
 let squareOfTheSum = (0...100).reduce(0, +).squared
 
 print(squareOfTheSum - sumOfTheSquares)
