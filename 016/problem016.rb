96c08db47d61838592394d9b3c735d0817168e1e
Frank Kair <frankkair@gmail.com>
2017-12-23 13:22:24 -0200

    Adds ruby 2.4 digits implementation as well

--- 016/problem016.rb
+++ 016/problem016.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts (2**1000).to_s.each_char.reduce(0) { |sum, char| sum + char.to_i }
+puts (2**1000).to_s.chars.reduce(0) { |sum, char| sum + char.to_i }
+
+# Ruby 2.4.2
+# puts (2**1000).digits.reduce(:+)
