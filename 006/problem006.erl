141b144cf5fc45a38c5c6e6230ab84c0772d79b3
Frank Kair <frankkair@gmail.com>
2018-01-26 23:55:01 -0200

    Adds Erlang problem006

---
+++ 006/problem006.erl
@@ -0,0 +1,15 @@
+-module(problem006).
+-export([solve/0]).
+
+square_of_sums() ->
+  L = lists:seq(1, 100),
+  S = lists:sum(L),
+  S * S.
+
+sum_of_squares() ->
+  L = lists:seq(1, 100),
+  S = lists:map(fun(X) -> X * X end, L),
+  lists:sum(S).
+
+solve() ->
+  square_of_sums() - sum_of_squares().
