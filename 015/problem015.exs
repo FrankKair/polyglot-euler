49435f40f7f498cb339fa1fb609de5a49cd192ef
Frank Kair <frankkair@gmail.com>
2018-01-30 00:06:15 -0200

    Refactors elixir problem015
    - Removes factorial(1), since base case factorial(0) is enough;
    - Adds guard clause (n > 0) to factorial;
    - Moves round function to combination.

--- 015/problem015.exs
+++ 015/problem015.exs
@@ -1,14 +1,16 @@
 #!/usr/bin/env elixir
 defmodule Problem015 do
   defp factorial(0), do: 1
-  defp factorial(1), do: 1
-  defp factorial(n), do: n * factorial(n - 1)
+  defp factorial(n) when n > 0, do: n * factorial(n - 1)
 
   # n = steps
   # k = grid size
-  defp combination(n, k), do: factorial(n) / (factorial(k) * factorial(n - k))
+  defp combination(n, k) do
+    combination = factorial(n) / (factorial(k) * factorial(n - k))
+    round(combination)
+  end
 
-  defp count_routes(k), do: round(combination(2 * k, k))
+  defp count_routes(k), do: combination(2 * k, k)
 
   def solve, do: count_routes(20)
 end
