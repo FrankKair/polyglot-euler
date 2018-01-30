02f09b23c3be9906a8896eb93411db9b246a7148
Frank Kair <frankkair@gmail.com>
2018-01-30 00:01:08 -0200

    Adds Erlang problem015

---
+++ 015/problem015.erl
@@ -0,0 +1,13 @@
+-module(problem015).
+-export([solve/0]).
+
+factorial(0) -> 1;
+factorial(N) when N > 0 -> N * factorial(N - 1).
+
+combination(N, K) -> 
+  C = factorial(N) / (factorial(K) * factorial(N - K)),
+  round(C).
+
+count_routes(K) -> combination(2 * K, K).
+
+solve() -> count_routes(20).
