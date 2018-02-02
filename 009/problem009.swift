5c57230f37c4aeedc654d0b5451e1d7c9e6c805b
Frank Kair <frankkair@gmail.com>
2018-02-02 00:09:54 -0200

    Adds swift problem009

---
+++ 009/problem009.swift
@@ -0,0 +1,24 @@
+#!/usr/bin/env xcrun swift
+extension Int {
+    var squared: Int { return self * self }
+}
+
+func isPythagorean(_ a: Int, _ b: Int, _ c: Int) -> Bool {
+    return a.squared + b.squared == c.squared
+}
+
+var product = 0
+
+for a in (1...500) {
+    for b in (1...500) {
+        let c = 1000 - a - b
+        if isPythagorean(a, b, c) {
+            let sum = a + b + c
+            if sum == 1000 {
+                product = a * b * c
+            }
+        }
+    }
+}
+
+print(product)
