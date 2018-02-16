8aac4d3f84d3855fd36ea47b958461cf22410f62
Frank Kair <frankkair@gmail.com>
2018-02-16 00:30:57 -0200

    Adds Elixir problem052

---
+++ 052/problem052.exs
@@ -0,0 +1,22 @@
+#!/usr/bin/env elixir
+defmodule Problem052 do
+  defp sorted_digits(x), do: x |> Integer.digits() |> Enum.sort()
+
+  defp condition(x) do
+    a = sorted_digits(x) == sorted_digits(x * 2)
+    b = sorted_digits(x * 3) == sorted_digits(x * 4)
+    c = sorted_digits(x * 5) == sorted_digits(x * 6)
+    a && b && c
+  end
+
+  defp numbers(), do: Stream.unfold(1, fn(x) -> {x, x+1} end)
+
+  def solve do
+    numbers()
+    |> Stream.take_while(&(!condition(&1)))
+    |> Enum.max
+    |> Kernel.+(1)
+  end
+end
+
+IO.puts Problem052.solve
