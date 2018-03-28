fa6e9a004f91153f0f99a208094f441adc279b12
Murilo Camargos <murilo@ckl.io>
2018-03-28 20:30:53 -0300

    Adds python problem005

---
+++ src/005/problem005.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+def gcd(a, b):
+    while b:      
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+x = lcm(1, 2)
+for i in range(3, 21):
+	x = lcm(x, i)
+
+print(x)
