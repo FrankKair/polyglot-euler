54eda1a9b17a9e2875a50bff81e7270e1be3ff7b
Murilo Camargos <murilo@ckl.io>
2018-03-28 20:30:27 -0300

    Adds python problem002

---
+++ src/002/problem002.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from math import sqrt
+
+# Fast Fibonacci Number
+# http://mathworld.wolfram.com/FibonacciNumber.html
+fib = lambda n: int(((1 + sqrt(5))**n - (1 - sqrt(5))**n)/(2**n * sqrt(5)))
+
+n = 2
+s = 0
+f = fib(n)
+while f < 4e6:
+    if f % 2 == 0:
+        s += f
+    n += 1
+    f = fib(n)
+
+print(s)
