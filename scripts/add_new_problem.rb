330fb68a284b51d11a225503168cd169301e6e65
Frank Kair <frankkair@gmail.com>
2018-03-30 14:06:51 -0300

    Update: Fixes add_new_problem script
    - Adds problem's url to content header;
    - Checks user input before creating directories.

--- scripts/add_new_problem.rb
+++ scripts/add_new_problem.rb
@@ -2,19 +2,40 @@
 require 'open-uri'
 require 'nokogiri'
 
+def valid_input?(n)
+  unless n
+    puts 'Specify a problem, like 001, 099, etc'
+    return false
+  end
+
+  if n.length != 3
+    puts 'Directory name must have 3 digits'
+    puts 'Like so: 001, 015, 050, 110, etc'
+    return false
+  end
+
+  true
+end
+
 def parse_euler_doc(n)
   base_url = 'https://projecteuler.net/problem'
   url = "#{base_url}=#{n}"
   Nokogiri::HTML(open(url))
 end
 
-def invalid_url?(n)
+def valid_url?(n)
   doc = parse_euler_doc(n)
-  doc.css('.problem_content').empty?
+  if doc.css('.problem_content').empty?
+    puts 'This is not a valid Project Euler problem.'
+    puts 'Check https://projecteuler.net/archives'
+    return false
+  end
+
+  true
 end
 
 def fetch_content(n)
-  content = "#{url}\n\n"
+  content = "https://projecteuler.net/problem=#{n}\n\n"
 
   doc = parse_euler_doc(n)
   doc.css('.problem_content').each do |node|
@@ -52,12 +73,7 @@ end
 # Main
 
 problem_number = ARGV[0]
-
-if invalid_url?(problem_number)
-  puts 'This is not a valid Project Euler problem.'
-  puts 'Check https://projecteuler.net/archives'
-  exit
-end
-
+exit unless valid_input?(problem_number)
+exit unless valid_url?(problem_number)
 create_dir(problem_number)
 create_readme_file(problem_number)
