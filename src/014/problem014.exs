d9533408aab36b9bf667eef6e00a67c55c5b59fc
Frank Kair <frankkair@gmail.com>
2018-03-30 13:39:56 -0300

    Adds Elixir problem014

---
+++ src/014/problem014.exs
@@ -0,0 +1,15 @@
+#!/usr/bin/env elixir
+defmodule Problem014 do
+  defp collatz_sequence(n, l) when n <= 1, do: l
+  defp collatz_sequence(n, l) when n > 1 do
+    if rem(n, 2) == 0 do
+      collatz_sequence(round(n / 2), l + 1)
+    else
+      collatz_sequence(round((3 * n) + 1), l + 1)
+    end
+  end
+
+  def solve(), do: Enum.max_by(2..1_000_000, fn(n) -> collatz_sequence(n, 0) end)
+end
+
+IO.puts Problem014.solve
