c0202774996d6b3833086e19b583d8675fbdf7d6
Frank Kair <frankkair@gmail.com>
2018-04-02 12:11:57 -0300

    Fix: Ruby 020
    - Removes unnecessary clause in factorial definition;
    - Uses to_s.chars.map(&:to_i) instead of digits (performance reasons).

--- src/020/problem020.rb
+++ src/020/problem020.rb
@@ -2,19 +2,12 @@
 class Integer
   def factorial
     return 1 if zero?
-    return 1 if self == 1
     self * (self - 1).factorial
   end
 end
 
-# Ruby 2.4.2
 def sum_factorial_digits(x)
-  x.factorial.digits.reduce(:+)
+  x.factorial.to_s.chars.map(&:to_i).reduce(:+)
 end
-
-# Ruby 2.2.3
-# def sum_factorial_digits(x)
-#   x.factorial.to_s.chars.map(&:to_i).reduce(:+)
-# end
 
 puts sum_factorial_digits(100)
