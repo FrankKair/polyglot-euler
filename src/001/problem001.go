4b699af41608a1549ce8212186ee0d1132db9add
Frank Kair <frankkair@gmail.com>
2018-04-14 13:55:05 -0300

    Feat: Adds Go problem001

---
+++ src/001/problem001.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func solve() int {
+    sum := 0
+    for i := 0; i < 1000; i++ {
+        if i%3 == 0 || i%5 == 0 {
+            sum += i
+        }
+  }
+  return sum
+}
+
+func main() {
+    result := solve()
+    fmt.Println(result)
+}
