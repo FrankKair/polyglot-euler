c355cab6b4cc638890b7876de485fcf5559a264b
Frank Kair <frankkair@gmail.com>
2018-04-17 08:19:01 -0300

    Feat: Adds Go 002

---
+++ src/002/002.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func solve() int {
+    a := 1
+    b := 2
+    sum := 0
+
+    for b < 4000000 {
+        if b%2 == 0 {
+            sum += b
+        }
+        a, b = b, a + b
+    }
+    return sum
+}
+
+func main() {
+    result := solve()
+    fmt.Println(result)
+}
