6493b4bc9d9c69c71fcaac8120f6e863a708aa50
Frank Kair <frankkair@gmail.com>
2018-04-17 23:39:21 -0300

    Feat: Adds Go 006

---
+++ src/006/006.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func solve() int {
+    sumOfTheSquares, sum := 0, 0
+
+    for i := 1; i < 100; i++ {
+        sumOfTheSquares += i * i
+        sum += i
+    }
+
+    squareOfTheSum := sum * sum
+
+    return squareOfTheSum - sumOfTheSquares
+}
+
+func main() {
+    result := solve()
+    fmt.Println(result)
+}
