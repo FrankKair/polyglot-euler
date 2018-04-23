bb87ca67eb52e8c5fbfdcd08eff9f8f6942b97ee
Frank Kair <frankkair@gmail.com>
2018-04-23 07:52:12 -0300

    Feat: Adds Rust 007

---
+++ src/007/007.rs
@@ -0,0 +1,33 @@
+fn sieve_of_eratosthenes(bound: usize) -> Vec<i32> {
+    // 2 and odd numbers
+    let mut primes: Vec<bool> = (0..bound + 1)
+                                    .map(|num| num == 2 || num & 1 != 0)
+                                    .collect();
+
+    let mut num = 3usize;
+    while num * num <= bound {
+        let mut j = num * num;
+
+        while j <= bound {
+            primes[j] = false;
+            j += num;
+        }
+        num += 2;
+    }
+
+    primes
+        .into_iter()
+        .enumerate()
+        .skip(2)
+        .filter_map(|(index, val)| if val {Some(index as i32)} else {None})
+        .collect::<Vec<i32>>()
+}
+
+fn solve() -> i32 {
+    sieve_of_eratosthenes(10001).last().cloned().unwrap()
+}
+
+fn main() {
+    let result = solve();
+    println!("{}", result);
+}
