bd78ab40a3c818ba179ed8dc10dec02710f5f171
Frederico Jordan <fredericojordan@gmail.com>
2018-04-28 15:18:13 -0300

    Adding 004 elixir solution

---
+++ src/004/p004.exs
@@ -0,0 +1,28 @@
+#!/usr/bin/env elixir
+defmodule Problem004 do
+  defp reverse_number(x) do
+  	x
+    |> Integer.to_string()
+    |> String.reverse()
+    |> String.to_integer()
+  end
+  
+  defp is_palindrome(x), do: x == reverse_number(x)
+  
+  defp get_palindrome_multiples(x, a, b) do
+    a..b
+    |> Stream.map(&(&1 * x))
+    |> Stream.filter(&is_palindrome(&1))
+    |> Enum.to_list()
+  end
+
+  def solve do
+    100..999
+    |> Stream.map(&get_palindrome_multiples(&1, 100, 999))
+    |> Enum.to_list()
+    |> List.flatten()
+    |> Enum.max()
+  end
+end
+
+IO.inspect Problem004.solve
