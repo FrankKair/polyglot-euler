bd8430c8cd3d4dd10c6d2395749a3fa05253cbbe
Frank Kair <frankkair@gmail.com>
2018-04-29 11:55:37 -0300

    Refactor: Crystal 022 map_with_index
    Instead of using a Hash to create A=1, B=2,
    we use bytes - 64 to get A=1, B=2...
    
    Also, instead of using a each_with_index with
    an external variable to keep sum state, we use
    a map_with_index and sum for a more functional
    approach.

--- src/022/p022.cr
+++ src/022/p022.cr
@@ -1,18 +1,18 @@
-class String
-  def score : Int
-    alphabet = Hash.zip(('A'..'Z').to_a, (1..26).to_a)
-    self.gsub("\"", "").chars.reduce(0) { |sum, char| sum + alphabet[char] }
-  end
-end
-
-def names : Array(String)
+def names
   File.read("p022_names.txt").split(",").sort
 end
 
-total_score = 0
-names.each_with_index do |name, index|
-  score = (index + 1) * name.score
-  total_score += score
+def solve
+  name_scores = names.map_with_index do |name, index|
+    bytes = name.bytes
+                .map { |b| b.to_i32 - 64 }
+                .select { |b| b > 0 }
+                .sum
+
+    (index + 1) * bytes
+  end
+
+  name_scores.sum
 end
 
-puts total_score
+puts solve
