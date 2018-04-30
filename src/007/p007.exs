7be230b722ca43301ae8a23601d9ab22ff1e8b01
Frederico Jordan <fredericojordan@gmail.com>
2018-04-30 09:14:27 -0300

    Optimizing primes generator

--- src/007/p007.exs
+++ src/007/p007.exs
@@ -1,12 +1,8 @@
 #!/usr/bin/env elixir
 defmodule Problem007 do
-  defp divisors(x, n, divisors) when n == x, do: [ n | divisors ]
-  defp divisors(x, n, divisors) when rem(x, n) == 0, do: divisors(x, n+1, [n | divisors])
-  defp divisors(x, n, divisors), do: divisors(x, n+1, divisors)
-  defp divisors(1), do: [1]
-  defp divisors(x) when is_integer(x) and x > 0, do: divisors(x, 1, [])
-  
-  defp is_prime(x), do: divisors(x) == [x, 1]
+  defp is_prime(x) do
+    Enum.all?((2..round(:math.sqrt(x))), fn(n) -> rem(x, n) != 0 end)
+  end
 
   defp generate_primes() do
     Stream.iterate(2, &(&1+1))
