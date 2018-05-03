f37cb337653955664f6374fcd5c20701ee979b84
Frederico Jordan <fredericojordan@gmail.com>
2018-05-03 17:52:19 -0300

    Reordering functions

--- src/024/p024.exs
+++ src/024/p024.exs
@@ -3,6 +3,13 @@ defmodule Problem024 do
   defp factorial(1), do: 1
   defp factorial(x) when is_integer(x) and x > 1, do: x*factorial(x-1)
 
+  defp divider(list) do
+    list
+    |> Enum.count()
+    |> Kernel.-(1)
+    |> factorial
+  end
+
   defp permutate([head], _, acc), do: [head | acc] |> Enum.reverse() |> Integer.undigits()
   defp permutate(list, index, acc) do
     { i, tail } =
@@ -11,13 +18,6 @@ defmodule Problem024 do
     permutate(tail, rem(index, divider(list)), [ i | acc ])
   end
   defp permutate(list, index), do: permutate(list, index-1, [])
-
-  defp divider(list) do
-    list
-    |> Enum.count()
-    |> Kernel.-(1)
-    |> factorial
-  end
 
   def solve do
     permutate([0,1,2,3,4,5,6,7,8,9], 1_000_000)
