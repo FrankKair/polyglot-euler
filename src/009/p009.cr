7e940773e01b2ae124965dc291c09f07cec95f67
Frank Kair <frankkair@gmail.com>
2018-05-03 12:01:14 -0300

    Refactor: Crystal/Ruby 009
    - Uses result / solve;
    - Loops from 0 to 500 instead of 1000.

--- src/009/p009.cr
+++ src/009/p009.cr
@@ -8,12 +8,16 @@ def pythagorean?(a, b, c)
   a.squared + b.squared == c.squared
 end
 
-product = 0
-(0..1000).each do |a|
-  (a..1000).each do |b|
-    c = 1000 - a - b
-    product = a * b * c if pythagorean?(a, b, c) && a + b + c == 1000
+def solve
+  product = 0
+  (0..500).each do |a|
+    (a..500).each do |b|
+      c = 1000 - a - b
+      product = a * b * c if pythagorean?(a, b, c) && a + b + c == 1000
+    end
   end
+  product
 end
 
-puts product
+result = solve
+puts result
