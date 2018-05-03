15e2dd7ae8cb6bfe06bc1302dd7c96d100c36324
Frank Kair <frankkair@gmail.com>
2018-05-03 12:10:08 -0300

    Refactor: Loops from 0 to 500

--- src/009/p009.go
+++ src/009/p009.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func solve() int {
 	product := 0
-	for a := 0; a < 1000; a++ {
-		for b := a + 1; b < 1000; b++ {
+	for a := 0; a < 500; a++ {
+		for b := a + 1; b < 500; b++ {
 			c := 1000 - a - b
 			sum := a + b + c
 			if a*a+b*b == c*c && sum == 1000 {
