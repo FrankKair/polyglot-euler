5ec8ad89652b82f869284a17dd143bf54db35df5
Frank Kair <frankkair@gmail.com>
2018-05-08 00:00:08 -0300

    Feat: Adds Lua 009

---
+++ src/009/p009.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env lua
+function solve()
+  local product = 1
+  for a=0, 500 do
+    for b=a+1,500 do
+      c = 1000 - b - a
+      local sum = a + b + c
+      if a*a + b*b == c*c and sum == 1000 then
+        product = a * b * c
+      end
+    end
+  end
+  return product
+end
+
+local result = solve()
+print(result)
