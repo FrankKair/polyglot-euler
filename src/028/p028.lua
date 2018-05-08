f7b2166920ec029f2dc7c6d8354f5fd2cb2c1603
Frank Kair <frankkair@gmail.com>
2018-05-07 23:22:52 -0300

    Feat: Adds Lua 028

---
+++ src/028/p028.lua
@@ -0,0 +1,12 @@
+#!/usr/bin/env lua
+
+function solve()
+  local sum = 1
+  for i=3, 1001, 2 do
+    sum = sum + (4*i*i - (6*(i-1)))
+  end
+  return sum
+end
+
+local result = solve()
+print(result)
