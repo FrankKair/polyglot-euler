a7ddd73a371a7086c7159d13ed18d3b2201d4733
Frank Kair <frankkair@gmail.com>
2018-05-08 00:11:10 -0300

    Feat: Adds Java 028

---
+++ src/028/p028.java
@@ -0,0 +1,14 @@
+public class p028 {
+    private static int solve() {
+        int sum = 1;
+        for (int i = 3; i < 1003; i += 2) {
+            sum += 4*i*i - (6 * (i - 1));
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = solve();
+        System.out.println(result);
+    }
+}
