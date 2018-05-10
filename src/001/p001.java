efeb727b0cea74aab8603f0ce5ebe4196f52744c
Frank Kair <frankkair@gmail.com>
2018-05-10 08:15:13 -0300

    Feat: Adds Java 001

---
+++ src/001/p001.java
@@ -0,0 +1,16 @@
+public class p001 {
+    private static int solve() {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = solve();
+        System.out.println(result);
+    }
+}
