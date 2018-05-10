787b96e0325079ec30cf804f37bb376f06e7c2d3
Frank Kair <frankkair@gmail.com>
2018-05-10 08:15:27 -0300

    Feat: Adds Lua 001

---
+++ src/001/p001.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/env lua
+function solve()
+  local sum = 0
+  for i=1, 1000 do
+    if (i % 3 == 0 or i % 5 == 0) then
+      sum = sum + i
+    end
+  end
+  return sum
+end
+
+local result = solve()
+print(result)
