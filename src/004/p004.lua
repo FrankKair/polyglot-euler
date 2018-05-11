0af5a67ede7bc88900bf4efe97d1e9ef206872fe
Frank Kair <frankkair@gmail.com>
2018-05-11 10:03:03 -0300

    Feat: Adds Lua 004

---
+++ src/004/p004.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env lua
+function is_palindrome(n)
+  local str = tostring(n)
+  return str == string.reverse(str)
+end
+
+function solve()
+  local largest = 0
+  for a=0, 999 do
+    for b=0, 999 do
+      local product = a * b
+      if product > largest and is_palindrome(product) then
+        largest = product
+      end
+    end
+  end
+  return largest
+end
+
+local result = solve()
+print(result)
