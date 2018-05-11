4e8694e2a5d78392df91d862cf6ee372ca685213
Frederico Jordan <fredericojordan@gmail.com>
2018-05-11 16:34:15 -0300

    Formatting function name

--- src/028/p028.clj
+++ src/028/p028.clj
@@ -1,11 +1,11 @@
-(defn spiral_diag_sum [x]
+(defn spiral-diag-sum [x]
   (cond
     (= x 1) 1
     (= x 2) 10
     :else (+ (* 4 x x)
              (- (* 6 (- x 1)))
-             (spiral_diag_sum (- x 2)))))
+             (spiral-diag-sum (- x 2)))))
 
-(defn solve [] (spiral_diag_sum 1001))
+(defn solve [] (spiral-diag-sum 1001))
 
 (prn (solve))
