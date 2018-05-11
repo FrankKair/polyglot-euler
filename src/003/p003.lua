7e5fcf280919311451baad720981e250c9576f7f
Frank Kair <frankkair@gmail.com>
2018-05-11 11:43:13 -0300

    Feat: Adds Lua 003

---
+++ src/003/p003.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua
+function solve()
+    local n = 600851475143
+    local limit = n / 2
+    local i = 3
+
+    while i < limit do
+      while n % i == 0 do
+        n = n / i
+        limit = n / 2
+      end
+      i = i + 2
+    end
+    return math.floor(n)
+end
+
+
+local result = solve()
+print(result)
