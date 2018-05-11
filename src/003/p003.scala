98cc806b1d6c5fa9a8f9e31962eee39a8a9eb118
Frank Kair <frankkair@gmail.com>
2018-05-11 09:58:32 -0300

    Refactor: Scala 003
    - Removes unnecessary is_prime function;
    - Uses solve / result pattern.

--- src/003/p003.scala
+++ src/003/p003.scala
@@ -1,30 +1,23 @@
-import scala.math.sqrt
-
 object Main {
-  def is_prime(n: Int): Boolean = {
-    if (n == 2 || n == 3) return true
-    if (n % 2 == 0) return false
-
-    for (i <- 3 until sqrt(n).toInt by 2)
-      if (n % i == 0) return false
-
-    true
-  }
-
-  def main(args: Array[String]): Unit = {
+  def solve(): Int = {
     var n = 600851475143L
     var f = 2
-    var s = Set[Int]()
+    var set = Set[Int]()
 
     while (n > 1) {
       if (n % f == 0) {
         n = (n / f).toLong
-        s += f
+        set += f
       } else {
         f += 1
       }
     }
 
-    println(s.last)
+    set.last
+  }
+
+  def main(args: Array[String]): Unit = {
+    val result = solve()
+    println(result)
   }
 }
