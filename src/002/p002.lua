7aa3374023f28aaea9d5c1d75158ec2ae5dcdcf7
Frank Kair <frankkair@gmail.com>
2018-05-14 00:40:00 -0300

    Feat: Adds Lua 002

---
+++ src/002/p002.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env lua
+function sum_even_fibs_below(limit)
+  local sum = 0
+  local a = 1
+  local b = 2
+
+  while b < limit do
+    if b % 2 == 0 then
+      sum = sum + b
+    end
+    a, b = b, a + b
+  end
+  return sum
+end
+
+function solve()
+  return sum_even_fibs_below(4000000)
+end
+
+local result = solve()
+print(result)
