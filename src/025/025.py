694934246227bf936493d275c4b4727eced3de62
Frank Kair <frankkair@gmail.com>
2018-05-25 00:51:49 -0300

    Feat: Adds Python 025

---
+++ src/025/025.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def solve():
+    index = 3
+    a = 1
+    b = 2
+
+    while len(str(b)) < 1000:
+        a, b = b, a + b
+        index += 1
+
+    return index
+
+
+if __name__ == '__main__':
+    result = solve()
+    print(result)
