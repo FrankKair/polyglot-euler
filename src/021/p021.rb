7f174acc111b0cf03cedd3bd1ab52671978455bb
Frank Kair <frankkair@gmail.com>
2018-06-05 01:47:50 -0300

    Feat: Adds Ruby 021

---
+++ src/021/p021.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+class Integer
+  def factors
+    factors = [1]
+    upper_bound = Math.sqrt(self).to_i
+    (2..upper_bound).each do |x|
+      if self % x == 0
+        factors << (x)
+        factors << (self / x).to_i
+      end
+    end
+    factors
+  end
+
+  def amicable?
+    factors_sum = self.factors.reduce(:+)
+    return false if self == factors_sum
+    self == factors_sum.factors.reduce(:+)
+  end
+end
+
+def solve
+  (1..10000).select { |x| x.amicable? }.reduce(:+)
+end
+
+puts solve
