ff5d19e3401b7f4a92e531b186f573ee8154e35f
yvendruscolo <yvendruscolo@gmail.com>
2018-06-13 16:05:39 -0300

    named functions outside solve

--- src/009/p009.clj
+++ src/009/p009.clj
@@ -1,11 +1,11 @@
-; works in both clojure and clojurescript
+(defn square [x] (Math/pow x 2))
+
+(defn sqrt [x] (Math/sqrt x))
+
+(defn sum-1000? [x] (== 1000 (reduce + 0 x)))
 
 (defn solve []
-  (let [square #(* % %)
-        sqrt #(Math/sqrt %)
-        sum-1000? #(== 1000 (apply + %))
-        candidates (for [b (range 700 5 -1)
-                         a (range 4 b)]
+  (let [candidates (for [b (range 700 5 -1), a (range 4 b)]
                         [a b (sqrt (+ (square a) (square b)))])]
         (transduce (comp (filter sum-1000?)
                          cat
