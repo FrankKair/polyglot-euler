d76d39855955826dccf687f4634eca1daf1fb890
Frank Kair <frankkair@gmail.com>
2018-06-25 18:28:47 -0300

    Feat: Adds TS 001

---
+++ src/001/p001.ts
@@ -0,0 +1,12 @@
+function solve(): number {
+  let sum: number = 0
+  for (let i: number = 0; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i
+    }
+  }
+  return sum
+}
+
+const result = solve()
+console.log(result)
