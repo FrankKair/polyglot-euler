77b59c454f21097ce1e4def3426a57e7bfff9dce
Frank Kair <frankkair@gmail.com>
2018-07-30 10:28:50 -0300

    Removes unnecessary .map in Ruby 041

--- src/041/p041.rb
+++ src/041/p041.rb
@@ -6,7 +6,7 @@ def solve
   permutations_array = [1, 2, 3, 4, 5, 6, 7].permutation.to_a
 
   permutations_array.each do |p|
-    number = p.map { |k| k }.join('').to_i
+    number = p.join('').to_i
     largest_prime_pandigital = number if Prime.prime?(number)
   end
   largest_prime_pandigital
