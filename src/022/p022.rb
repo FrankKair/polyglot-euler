70e00dec00707fd0e7dc8f20a4418fb07dab2d39
Frank Kair <frankkair@gmail.com>
2018-09-03 21:42:46 +0100

    Feat: Adds Ruby 022

---
+++ src/022/p022.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+def names
+  File.read("p022_names.txt").split(",").sort
+end
+
+def solve
+  name_scores = names.each_with_index.map do |name, index|
+    bytes = name.bytes
+                .map { |b| b.to_i - 64 }
+                .select { |b| b > 0 }
+                .reduce(:+)
+                
+    (index + 1) * bytes
+  end
+name_scores.reduce(:+)
+end
+
+puts solve
