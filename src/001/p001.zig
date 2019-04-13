eda92713a46579d82c634402ab8482fc442b34e7
yvendruscolo <yvendruscolo@gmail.com>
2019-04-13 10:56:42 -0300

    inline condition

--- src/001/p001.zig
+++ src/001/p001.zig
@@ -1,15 +1,13 @@
 const std = @import("std");
 const warn = std.debug.warn;
 
-fn divby3or5(num: u64) bool {
-  return num % 3 == 0 or num % 5 == 0;
-}
-
 pub fn main() void {
   var sum: u64 = 0;
   var j: u64 = 3;
   while (j < 1000) : (j += 1) {
-    if (divby3or5(j)) {sum += j;}
+    if (j % 3 == 0 or j % 5 == 0) {
+      sum += j;
+    }
   } 
   warn("{}\n", sum);
 }
