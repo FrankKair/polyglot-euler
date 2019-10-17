87ff4118af58d3acbba1191f406432b00bb6e9b4
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2019-10-17 15:35:21 -0400

    Adding C++ 010

---
+++ src/010/p010.cpp
@@ -0,0 +1,43 @@
+//Euler Problem 10
+//Author: Ernulphus
+//What is the sum of all primes under 1 million?
+
+#include<iostream>
+
+bool isPrime(int x);
+int solve();
+
+int solve()
+{
+  unsigned long sum = 2;
+  int LAST_PRIME = 2000000;
+
+  int p = 3;
+  while (p < LAST_PRIME)
+  {
+    if (isPrime(p))
+    {
+      sum += p;
+      //std::cout << p << std::endl;
+    }
+    p++;
+  }
+  return sum;
+}
+
+bool isPrime(int x)
+{
+  for (int i = 2; i < x; i++)
+  {
+    if (x%i == 0) //i is a factor of x
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main()
+{
+  std::cout << solve() << std::endl;
+}
