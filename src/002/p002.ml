354fc488cc81f83f7a4d039571e5932393cf4dc3
Frank Kair <frankkair@gmail.com>
2020-02-09 12:45:53 +0000

    Adds p002 in OCaml

---
+++ src/002/p002.ml
@@ -0,0 +1,14 @@
+open Base
+open Stdio
+
+let solve =
+  let rec fib a b acc =
+    let next = a + b in
+    if next > 4000000 then acc
+    else if (next % 2) = 0 then fib b next (acc + next)
+    else fib b next acc
+  in
+    fib 1 1 0
+
+let () =
+  printf "%d\n" solve
