ecb9bf81e332d7caf5c9d0e0dea380b490df0292
Frank Kair <frankkair@gmail.com>
2020-02-21 23:53:09 +0000

    Feat: Adds Rust p030

---
+++ src/030/p030.rs
@@ -0,0 +1,23 @@
+fn digits(num: i32) -> Vec<i32> {
+    num.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .collect::<Vec<i32>>()
+}
+
+fn digits_fifth_power(n: i32) -> i32 {
+    digits(n).into_iter()
+             .map(|d| d.pow(5))
+             .sum()
+}
+
+fn solve() -> i32 {
+    (2..200000)
+        .filter(|&n| digits_fifth_power(n) == n)
+        .sum()
+}
+
+fn main() {
+    let result = solve();
+    println!("{:?}", result);
+}
