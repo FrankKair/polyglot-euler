9790ca8f98e5085bc750175f97af728860fe375e
Frank Kair <frankkair@gmail.com>
2020-02-21 23:52:33 +0000

    Feat: Adds OCaml p030

---
+++ src/030/p030.ml
@@ -0,0 +1,24 @@
+open Base
+open Stdio
+
+let digits n =
+  let rec loop n acc =
+    if n = 0 then acc
+    else loop (n / 10) (n % 10::acc) in
+  match n with
+  | 0 -> [0]
+  | _ -> loop n []
+
+let sum = List.fold ~init:(0) ~f:(+)
+
+let digits_fifth_power n =
+  List.map ~f:(fun x -> Int.pow x 5) (digits n)
+  |> sum
+
+let solve =
+  List.range 2 200000
+  |> List.filter ~f:(fun x -> digits_fifth_power x = x)
+  |> sum
+
+let () =
+  printf "%d\n" solve;;
