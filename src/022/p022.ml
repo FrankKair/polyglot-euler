848482abcfcabb311226d1542df5e6ae6bb84098
Frank Kair <frankkair@gmail.com>
2020-03-06 23:58:50 +0000

    Adds OCaml p022

---
+++ src/022/p022.ml
@@ -0,0 +1,34 @@
+(*
+dune
+
+(executable
+  (name p022)
+  (libraries base core_kernel str stdio))
+*)
+open Core_kernel
+open Stdio
+
+let load_names = 
+  let replace ~pattern ~with_str =
+    Str.global_replace (Str.regexp_string pattern) with_str in
+
+  In_channel.read_all "p022_names.txt"
+  |> replace ~pattern:"\"" ~with_str:""
+  |> String.split ~on:',' 
+  |> List.sort ~compare:(Stdlib.compare)
+
+let name_score index name = 
+  let char_val c = Char.to_int c - 64 in
+  name
+  |> Bytes.of_string
+  |> Bytes.to_list
+  |> List.map ~f:(fun c -> (index + 1) * char_val c)
+  |> List.fold ~f:(+) ~init:(0)
+
+let solve = 
+  load_names
+  |> List.mapi ~f:(fun i s -> name_score i s)
+  |> List.fold ~f:(+) ~init:(0)
+
+let () =
+  printf "%i\n" solve;;
