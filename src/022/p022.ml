734276132588dba01f6ec88909907a02da61a29e
Frank Kair <frankkair@gmail.com>
2020-03-07 00:47:20 +0000

    Refactors 022 with sum & mapi ~f:(name_score)
    - Also puts replace outside the scope of load_names function

--- src/022/p022.ml
+++ src/022/p022.ml
@@ -1,14 +1,16 @@
 open Core_kernel
 open Stdio
 
-let load_names = 
-  let replace ~pattern ~with_str =
-    Str.global_replace (Str.regexp_string pattern) with_str in
+let replace ~pattern ~with_str =
+  Str.global_replace (Str.regexp_string pattern) with_str
 
+let load_names = 
   In_channel.read_all "p022_names.txt"
   |> replace ~pattern:"\"" ~with_str:""
   |> String.split ~on:',' 
   |> List.sort ~compare:(Stdlib.compare)
+
+let sum = List.fold ~f:(+) ~init:(0)
 
 let name_score index name = 
   let char_val c = Char.to_int c - 64 in
@@ -16,12 +18,12 @@ let name_score index name =
   |> Bytes.of_string
   |> Bytes.to_list
   |> List.map ~f:(fun c -> (index + 1) * char_val c)
-  |> List.fold ~f:(+) ~init:(0)
+  |> sum
 
 let solve = 
   load_names
-  |> List.mapi ~f:(fun i s -> name_score i s)
-  |> List.fold ~f:(+) ~init:(0)
+  |> List.mapi ~f:(name_score)
+  |> sum
 
 let () =
   printf "%i\n" solve;;
